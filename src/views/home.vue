a66ae74a8f08155d21544f605df1bfc31940ba3e
connectshark <bobhus394@gmail.com>
2021-09-15 17:58:50 +0800

    刪除多於內容

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,5 +1,5 @@
 <template>
-  <h2 class="title">第一頁由此開始</h2>
+<div class="home"></div>
 </template>
 
 <script>
@@ -11,6 +11,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss" scoped>
-</style>
